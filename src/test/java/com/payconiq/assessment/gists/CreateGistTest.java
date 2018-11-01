befe4b00887e123d563ea3de4e785df36c22331c
Nicolae <danu.nicu@gmail.com>
2018-11-01 11:29:06 +0100

    update test assertions and  extract static fields.

--- src/test/java/com/payconiq/assessment/gists/CreateGistTest.java
+++ src/test/java/com/payconiq/assessment/gists/CreateGistTest.java
@@ -5,16 +5,22 @@ import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class CreateGistTest extends TestBase {
+    private static final String GIST_CONTENT = "Having a POJO would have been way much easier";
+    private static final String DOC_CREATE_GIST = "#create-a-gist";
+
     @Test
     public void createPublicGistWithOneFileIsOk(){
         auth()
             .body(getFileFromResources("createGistOne.json"))
             .post(GISTS).then().statusCode(HttpStatus.SC_CREATED)
-            .body("owner.login",is("payconiqQA"));
+            .body("owner.login",is(OWNER),
+                    "description",is("Created gistOne via API"),
+                    "public",is(true),
+                    "files.size()",is(1),
+                    "files['gistOne.txt'].content",is(GIST_CONTENT));
     }
 
     @Test
@@ -22,7 +28,11 @@ public class CreateGistTest extends TestBase {
         auth()
                 .body(getFileFromResources("createGistTwo.json"))
                 .post(GISTS).then().statusCode(HttpStatus.SC_CREATED)
-                .body("owner.login",is("payconiqQA"));
+                .body("owner.login",is(OWNER),
+                        "description",is("Created gistTwo via API"),
+                        "public",is(false),
+                        "files.size()",is(1),
+                        "files['gistTwo.txt'].content",is(GIST_CONTENT));
     }
 
     @Test
@@ -30,7 +40,14 @@ public class CreateGistTest extends TestBase {
         auth()
                 .body(getFileFromResources("createGistMultipleFiles.json"))
                 .post(GISTS).then().statusCode(HttpStatus.SC_CREATED)
-                .body("owner.login",is("payconiqQA"));
+                .body("owner.login",is(OWNER),
+                        "description",is("Created multiple gist files via API"),
+                        "public",is(true),
+                        "files.size()",both(greaterThanOrEqualTo(0)).and(lessThan(Integer.MAX_VALUE)),
+                        "files['README.md'].content",is(GIST_CONTENT),
+                        "files['README.md'].language",is("Markdown"),
+                        "files['README.txt'].language",is("Text"),
+                        "files['README.yaml'].language",is("YAML"));
     }
 
     @Test
@@ -38,8 +55,9 @@ public class CreateGistTest extends TestBase {
         auth()
                 .body("invalidPayload")
                 .post(GISTS).then().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .header("X-GitHub-Media-Type",containsString("github.v3"))
                 .body("message",is("Problems parsing JSON"),
-                        "documentation_url",containsString("#create-a-gist"));
+                        "documentation_url",containsString(DOC_CREATE_GIST));
     }
     @Test
     public void createGistWithIncorrectUriIsNotOk(){
@@ -56,6 +74,6 @@ public class CreateGistTest extends TestBase {
                 .body(getFileFromResources("createGistOne.json"))
                 .post(GISTS).then().statusCode(HttpStatus.SC_UNAUTHORIZED)
                 .body("message",is("Requires authentication"),
-                        "documentation_url",containsString("#create-a-gist"));
+                        "documentation_url",containsString(DOC_CREATE_GIST));
     }
 }
